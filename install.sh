fbde0fc962f092da8e41873dff5c0afa79d24838
Benjamin Koffel <benjaminkoffel@gmail.com>
2018-04-12 17:14:05 +1000

    update

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
+secret=$1
+
 apt-get update
 apt-get install -y dirmngr apt-transport-https --install-recommends
 
-echo 'deb https://dl.bintray.com/cert-bdf/debian any main' | tee -a /etc/apt/sources.list.d/thehive-project.list
-apt-key adv --keyserver hkp://pgp.mit.edu --recv-key 562CBC1C
-
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key D88E42B4
-echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-5.x.list
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" > /etc/apt/sources.list.d/elastic-5.x.list
+
+apt-key adv --keyserver hkp://pgp.mit.edu --recv-key 562CBC1C
+echo 'deb https://dl.bintray.com/cert-bdf/debian any main' > /etc/apt/sources.list.d/thehive-project.list
 
 apt-get update
-apt-get install thehive cortex elasticsearch
+apt-get install -y elasticsearch thehive cortex 
 
+echo "play.crypto.secret=\"$secret\"" >> /etc/thehive/application.conf
 cp elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
 
-systemctl enable elasticsearch.service
-systemctl start elasticsearch.service
-systemctl status elasticsearch.service
+service elasticsearch start
+service thehive start
+service cortex start
