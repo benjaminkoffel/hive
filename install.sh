3ebfbb1a98b00a41dcd84a8e2fe45d91e097b61a
Benjamin Koffel <benjaminkoffel@gmail.com>
2018-04-12 16:26:12 +1000

    initial commit

---
+++ install.sh
@@ -0,0 +1,16 @@
+# hive/cortex
+echo 'deb https://dl.bintray.com/cert-bdf/debian any main' | sudo tee -a /etc/apt/sources.list.d/thehive-project.list
+apt-key adv --keyserver hkp://pgp.mit.edu --recv-key 562CBC1C
+apt-get update
+apt-get install thehive cortex
+
+# elasticsearch
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key D88E42B4
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
+apt install apt-transport-https
+apt update && sudo apt install elasticsearch
+cp elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+systemctl enable elasticsearch.service
+systemctl start elasticsearch.service
+systemctl status elasticsearch.service
+
