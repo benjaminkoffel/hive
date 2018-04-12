67232edcb8d521dbfa49408c8ae707984a416dc7
Benjamin Koffel <benjaminkoffel@gmail.com>
2018-04-12 17:51:40 +1000

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 secret=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
 
 apt-get update
-apt-get install -y dirmngr apt-transport-https --install-recommends
+apt-get install -y dirmngr apt-transport-https git --install-recommends
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key D88E42B4
 echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" > /etc/apt/sources.list.d/elastic-5.x.list
@@ -12,12 +12,18 @@ echo 'deb https://dl.bintray.com/cert-bdf/debian any main' > /etc/apt/sources.li
 apt-get update
 apt-get install -y elasticsearch thehive cortex 
 
+cp elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
 echo "play.crypto.secret=\"$secret\"" >> /etc/thehive/application.conf
 echo "play.http.secret.key=\"$secret\"" >> /etc/cortex/application.conf
-cp elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+cd /opt/
+git clone https://github.com/TheHive-Project/Cortex-Analyzers.git
 
-service elasticsearch start
-service thehive start
-service cortex start
+# service elasticsearch start
+# service thehive start
+# service cortex start
 
-echo "CONFIGURED SECRET: $secret"
+echo "MANUAL:"
+echo "- open cortex http://localhost:9001"
+echo "- create organization, user and API key'"
+echo "- edit /etc/thehive/application.conf with API key"
+echo "- edit /etc/cortex/application.conf with /opt/Cortex-Analyzers/analyzers"
