d3dadfdf84870a91a5e871eb8cec9b133f0093e3
Benjamin Koffel <benjaminkoffel@gmail.com>
2018-04-12 22:47:51 +1000

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get install -y dirmngr apt-transport-https git python2-pip python3-pip --install-recommends
+apt-get install -y dirmngr apt-transport-https git python-pip python3-pip --install-recommends
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key D88E42B4
 echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" > /etc/apt/sources.list.d/elastic-5.x.list
